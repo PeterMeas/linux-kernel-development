5894fac8b53b46358f512d2cfcddab1ab5dede2d
Peter Meas <peter_meas@fall2025-uml.kdlp.underground.software>
2025-09-23 20:11:44 +0000

    new_syscall: Add assembly helper for kdlp syscall and updated test program
    Signed-off-by: Peter Meas <peter_meas@fall2025-uml.kdlp.underground.software>

--- peter_meas/new_syscall/test.c
+++ peter_meas/new_syscall/test.c
@@ -3,19 +3,27 @@
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <linux/reboot.h>
+#include <sys/reboot.h>
+
+extern long kdlp(const char *buf, int size);
 
 int main(int argc, char** argv) {
 
 	char buff[128];
-	long numBytes;
+	long ret;
 
-	numBytes = syscall(__NR_kdlp, buff, sizeof(buff));
-	if (numBytes < 0) {
+	ret = kdlp(buff, sizeof(buff));
+	//ret = syscall(__NR_kdlp, buff, sizeof(buff));
+	if (ret < 0) {
 
-		printf("Syscall failed: %s\n", strerror(-numBytes));
-		return -numBytes;
+	//	printf("Syscall failed: %s\n", strerror(-ret));
+	//	return -ret;
+		printf("Error: syscall failed with code %ld\n", -ret);
 	} else {
-		write(1, buff, numBytes);
+		printf("Syscall succeeded, returned %ld\n", ret);
+	//	write(1, buff, ret);
 	}
+	reboot(LINUX_REBOOT_CMD_POWER_OFF);
 	return 0;
 }
